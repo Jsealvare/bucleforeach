9e14af72064ef55d580b0a1f575c2c5925fae271
Jsealvare <144851161+Jsealvare@users.noreply.github.com>
2023-10-03 16:22:29 -0500

    Update script.js
    correcion para buen funcionamiento

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const letters=["a","b","c","d"];
 //con funciones por expresion 
-const f=funtion() {
+const f=function() {
   console.log("un elemento.");
-
-
-}
-letters.forEach(f):
+};
+letters.forEach(f);
 
 //con funciones anonimas 
-letters.forEach(funcion(){console.log("un elemento");});
+letters.forEach(function(){
+  console.log("un elemento");
+});
 
 //con funciones feclecha 
 letters.forEach(()=>console.log("un elemento"));
