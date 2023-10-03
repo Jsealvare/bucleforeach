b3f053851117f44dc235ee8f66929ff7346d2a5c
Jsealvare <144851161+Jsealvare@users.noreply.github.com>
2023-10-03 16:02:09 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const letters=["a","b","c","d"];
+//con funciones por expresion 
+const f=funtion() {
+  console.log("un elemento.");
+
+
+}
+letters.forEach(f):
+
+//con funciones anonimas 
+letters.forEach(funcion(){console.log("un elemento");});
+
+//con funciones feclecha 
+letters.forEach(()=>console.log("un elemento"));
